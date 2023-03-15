c8fa676936c35c01d8f9e60d2fb27b88dbac1e68
ruligandari <ruligandari@gmail.com>
2023-03-15 13:58:41 +0700

    kemana

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Parentbox">
+      <FotoProduct />
+      <ProductInfo isDiscount="coming" category="LEBARAN" name="Adadeh" />
+    </div>
+  );
+}
+function FotoProduct() {
+  return (
+    <div className="Foto">
+      <img src="sneaker.jpeg" alt="Foto" />
     </div>
   );
 }
 
+function CheckDiscount(props) {
+  const { isDiscount } = props;
+  if (isDiscount == "yes") {
+    return <p> Diskon 50% Off</p>;
+  } else if (isDiscount == "coming") {
+    return <p>akan Ada Diskon</p>;
+  }
+}
+
+function ProductInfo(props) {
+  const { category, name, isDiscount } = props;
+  const benefits = [
+    "Tidak kusut Terkena Air",
+    "Tidak kusut Jika diinjak",
+    "Tidak akan rusak",
+    "Tidak kusut Terkena Air",
+    "Tidak kusut Jika diinjak",
+    "Tidak akan rusak",
+  ];
+  const listBenefits = benefits.map((itemBenefit) => <li>{itemBenefit}</li>);
+  return (
+    <div className="Deskripsi">
+      <p className="Cate">{category}</p>
+      <h1 className="Title">{name}</h1>
+      <p className="Price">IDR 700.000</p>
+      <CheckDiscount isDiscount={isDiscount} />
+      <p className="Info">
+        Sneaker pinky swag ini sangat cocok untuk kamu yang ingin tampil
+      </p>
+      {listBenefits}
+      <a onClick={(e) => Tambahcart(name, e)} href="#">
+        Add to Cart
+      </a>
+    </div>
+  );
+}
+
+function Tambahcart(e) {
+  return console.log("Tambah cart " + e);
+}
 export default App;
